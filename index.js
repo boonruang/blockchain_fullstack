38d6e0296f1474de6d1c216a059834a60c987ecb
boonruang <scubatoy@gmail.com>
2022-02-26 08:50:43 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+class Block {
+  constructor(data, hash, lastHash) {
+    this.data = data
+    this.hash = hash
+    this.lastHash = lastHash
+  }
+}
+
+const fooBlock = new Block('foo-data', 'foo-hash', 'foo-lastHash')
+console.log(fooBlock)
