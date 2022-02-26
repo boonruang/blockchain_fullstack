f735c13c1311169b4b5a3491b579daebb7fad43b
boonruang <scubatoy@gmail.com>
2022-02-26 10:57:36 +0700

    Ep11 Mine Blocks

--- block.test.js
+++ block.test.js
@@ -1,5 +1,3 @@
-// const { it, expect } = require('@jest/globals')
-// const { describe } = require('yargs')
 const Block = require('./block')
 const { GENESIS_DATA } = require('./config')
 
@@ -26,14 +24,34 @@ describe('Block', () => {
   describe('genesis()', () => {
     const genesisBlock = Block.genesis()
 
-    console.log('genesisBlock', genesisBlock)
-
     it('returns a Block instance', () => {
       expect(genesisBlock instanceof Block).toBe(true)
     })
 
     it('returns the genesis data', () => {
       expect(genesisBlock).toEqual(GENESIS_DATA)
+    })
+  })
+
+  describe('mineBlock()', () => {
+    const lastBlock = Block.genesis()
+    const data = 'mined data'
+    const minedBlock = Block.mineBlock({ lastBlock, data })
+
+    it('returns a Block instance', () => {
+      expect(minedBlock instanceof Block).toBe(true)
+    })
+
+    it('sets the `lastHash` to be the `hash` of the lastBlock', () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash)
+    })
+
+    it('sets the `data`', () => {
+      expect(minedBlock.data).toEqual(data)
+    })
+
+    it('sets the `timestamp`', () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined)
     })
   })
 })
