21750f4af34ab2524a884e6b3fc10a92751f26f3
boonruang <scubatoy@gmail.com>
2022-03-01 00:59:15 +0700

    Ep19 Chain Replacement"

--- crypto-hash.js
+++ crypto-hash.js
@@ -4,7 +4,6 @@ const cryptoHash = (...inputs) => {
   const hash = crypto.createHash('sha256')
 
   hash.update(inputs.sort().join(''))
-  console.log('input: ', inputs)
   return hash.digest('hex')
 }
 
