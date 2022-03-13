75075e939a686e3ded8564f4c236db5dc49db508
boonruang <scubatoy@gmail.com>
2022-03-13 18:55:15 +0700

    เห็น64. Transaction Update in the API

--- index.js
+++ index.js
@@ -31,13 +31,19 @@ app.post('/api/mine', (req, res) => {
   res.redirect('/api/blocks')
 })
 
-app.post('/api/transac', (req, res) => {
+app.post('/api/transact', (req, res) => {
   const { amount, recipient } = req.body
 
-  let transaction
+  let transaction = transactionPool.existingTransaction({
+    inputAddress: wallet.publicKey,
+  })
 
   try {
-    transaction = wallet.createTransaction({ recipient, amount })
+    if (transaction) {
+      transaction.update({ senderWallet: wallet, recipient, amount })
+    } else {
+      transaction = wallet.createTransaction({ recipient, amount })
+    }
   } catch (error) {
     return res.status(400).json({ type: 'error', message: error.message })
   }
