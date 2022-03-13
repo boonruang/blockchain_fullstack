e964472ff46a8f767440d795b2a7d32cbe380e77
boonruang <scubatoy@gmail.com>
2022-03-13 21:47:43 +0700

    67. Pubnub Set Transaction Edge Case

--- app/pubsub.js
+++ app/pubsub.js
@@ -7,9 +7,10 @@ const CHANNELS = {
 }
 
 class PubSub {
-  constructor({ blockchain, transactionPool }) {
+  constructor({ blockchain, transactionPool, wallet }) {
     this.blockchain = blockchain
     this.transactionPool = transactionPool
+    this.wallet = wallet
     this.publisher = redis.createClient()
     this.subscriber = redis.createClient()
 
@@ -30,7 +31,13 @@ class PubSub {
         this.blockchain.replaceChain(parsedMessage)
         break
       case CHANNELS.TRANSACTION:
-        this.transactionPool.setTransaction(parsedMessage)
+        if (
+          !this.transactionPool.existingTransaction({
+            inputAddress: this.wallet.publicKey,
+          })
+        ) {
+          this.transactionPool.setTransaction(parsedMessage)
+        }
         break
       default:
         return
