95d94b07e6c804fc47a8619d47c882a1945f9247
boonruang <scubatoy@gmail.com>
2022-03-14 14:28:43 +0700

    72. Grab Valid Transactions

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,4 @@
+const Transaction = require('./transaction')
 class TransactionPool {
   constructor() {
     this.transactionMap = {}
@@ -17,6 +18,12 @@ class TransactionPool {
     return transactions.find(
       (transaction) => transaction.input.address === inputAddress,
     )
+  }
+
+  validTransactions() {
+    return Object.values(this.transactionMap).filter((transaction) => {
+      Transaction.validTransaction(transaction)
+    })
   }
 }
 
