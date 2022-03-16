fea4c0fb27819e9b9a1d5e2285ae496ae18f7a60
boonruang <scubatoy@gmail.com>
2022-03-16 14:26:41 +0700

    96. App Component

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-render(<div>Cryptochain in React!!!</div>, document.getElementById('root'))
+render(<div>Welcome to the blockchain!!!</div>, document.getElementById('root'))
