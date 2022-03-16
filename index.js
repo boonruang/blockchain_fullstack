11a14efdf4fc686112e00f56b4974385db59b3c0
boonruang <scubatoy@gmail.com>
2022-03-16 13:27:35 +0700

    92. Serve a Frontend Page

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const request = require('request')
+const path = require('path')
 const Blockchain = require('./blockchain')
 const PubSub = require('./app/pubsub')
 const TransactionPool = require('./wallet/transaction-pool')
@@ -85,6 +86,10 @@ app.get('/api/wallet-info', (req, res) => {
       address,
     }),
   })
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/index.html'))
 })
 
 const syncWithRootState = () => {
