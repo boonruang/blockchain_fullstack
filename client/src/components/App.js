f2cd236961a6b8a91bc35eb5814b21148fc34747
boonruang <scubatoy@gmail.com>
2022-03-18 00:31:02 +0700

    98. [Optional] Upgrade to Babel Version 7

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,12 +3,11 @@ import React, { Component } from 'react'
 class App extends Component {
   state = { walletInfo: { address: 'fooxb6', balance: 9999 } }
 
-  //   componentDidMount() {
-  //     fetch('http://localhost:3000/api/wallet-info')
-  //       .then((response) => response.json())
-  //       //   .then((json) => console.log('json', json))
-  //       .then((json) => this.setState({ walletInfo: json }))
-  //   }
+  componentDidMount() {
+    fetch('http://localhost:3000/api/wallet-info')
+      .then((response) => response.json())
+      .then((json) => this.setState({ walletInfo: json }))
+  }
 
   render() {
     const { address, balance } = this.state.walletInfo
--- readme.txt
+++ readme.txt
@@ -34,3 +34,9 @@ npm run build-client
 npm run dev
 
 npm i babel-core@6.26.3 babel-plugin-transform-class-properties@6.24.1 babel-plugin-transform-object-rest-spread@6.26.0 babel-preset-env@1.7.0 babel-preset-react@6.24.1 --save
+
+remove all babel related (above)
+
+npm i @babel/core@7.2.2 --save-dev
+
+delete .babelrc
