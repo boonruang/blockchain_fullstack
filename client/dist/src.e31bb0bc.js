ca2c05d6a55d603e5213f814ee82e128f99ea949
boonruang <scubatoy@gmail.com>
2022-03-19 02:06:39 +0700

    106. Transaction Component

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -49234,7 +49234,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1236" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "15245" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
